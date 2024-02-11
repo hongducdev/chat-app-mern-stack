5e09aa0d2c821ced3a69bd53058488dca1d7e8c2
hongducdev <contact.hongduc@gmail.com>
2024-02-11 22:13:08 +0700

    Init project and config router backend

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,13 @@
+export const signup = (req, res) => {
+  console.log("Signup User");
+  res.send("Signup User");
+};
+export const login = (req, res) => {
+  console.log("Login User");
+  res.send("Login User");
+};
+
+export const logout = (req, res) => {
+  console.log("Logout User");
+  res.send("Logout User");
+};
