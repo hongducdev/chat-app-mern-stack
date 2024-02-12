c04efbd21a0fca8ee9a9c4149725e9bffc2449ac
hongducdev <contact.hongduc@gmail.com>
2024-02-12 20:19:45 +0700

    Login Endpoint

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -46,9 +46,36 @@ export const signup = async (req, res) => {
     });
   }
 };
-export const login = (req, res) => {
-  console.log("Login User");
-  res.send("Login User");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ error: "Account does not exist" });
+    }
+
+    const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ error: "Password is incorrect" });
+    }
+
+    await generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
 };
 
 export const logout = (req, res) => {
