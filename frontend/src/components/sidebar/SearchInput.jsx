067f77e9f7e8a4ac28a82da4a0c4b3229c3249e6
hongducdev <contact.hongduc@gmail.com>
2024-02-14 09:29:47 +0700

    Design Message container: Message input, message bubble

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -5,7 +5,7 @@ const SearchInput = () => {
       <input
         type="text"
         placeholder="Search for a user"
-        className="input input-bordered rounded-full"
+        className="input input-bordered rounded-full bg-ctp-mantle"
       />
       <button className="h-12 w-12 rounded-full bg-gradient-to-r from-ctp-pink to-ctp-mauve text-ctp-base flex items-center justify-center">
         <RiSearch2Line className="w-6 h-6" />
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
+import MessageContainer from "../../components/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
   return (
     <div className="flex sm:h-[450px] md:h-[550px] overflow-hidden rounded-lg shadow-md bg-gradient-to-br from-ctp-mantle to-ctp-crust outline outline-ctp-pink">
       <Sidebar />
+      <MessageContainer />
     </div>
   );
 };
