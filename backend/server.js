da3a60b0a76f40cc229fffefdf0c9b7b533d7e0f
hongducdev <contact.hongduc@gmail.com>
2024-02-16 16:40:48 +0700

    Add Socket.io in FrontEnd and BackEnd

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import userRoutes from "./routes/user.route.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
-app.use(express.json());
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
   connectToMongoDB();
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server Running on port ${PORT}`);
 });
