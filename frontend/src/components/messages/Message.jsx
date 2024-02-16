b9c244fa31dbd886462c5b66dc0fa1427809b3ae
hongducdev <contact.hongduc@gmail.com>
2024-02-16 17:16:10 +0700

    Show message when have new message

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -2,6 +2,7 @@ import propTypes from "prop-types";
 import { useAuthContext } from "../../context/AuthContext";
 import useConversation from "../../store/useConversation";
 import { extractTime } from "../../utils/extractTime";
+import { useEffect } from "react";
 
 const Message = ({ message }) => {
   const { authUser } = useAuthContext();
@@ -13,6 +14,30 @@ const Message = ({ message }) => {
   const profilePic = fromMe
     ? authUser.profilePic
     : selectedConversation?.profilePic;
+
+  useEffect(() => {
+    // Xin cấp quyền hiển thị thông báo chỉ một lần khi component được tạo
+    const requestNotificationPermission = async () => {
+      if (Notification.permission !== "granted") {
+        await Notification.requestPermission();
+      }
+    };
+
+    requestNotificationPermission();
+  }, []);
+
+  useEffect(() => {
+    if (message.notification && !fromMe) {
+      const notification = new Notification("New message", {
+        body: message.message,
+        icon: profilePic,
+      });
+      notification.onclick = () => {
+        window.focus();
+        notification.close();
+      };
+    }
+  }, [message, fromMe, profilePic]);
 
   return (
     <div className={`chat ${chatClass}`}>
